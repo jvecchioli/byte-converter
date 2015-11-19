50d0073da6e5a833efcccdcdf025b091e6a9304b
jvecchioli <jvecchioli@gsx.com>
2015-11-19 13:57:56 +0100

    issue#1: fix conversion between KB and B

--- test/test-converterBase10.js
+++ test/test-converterBase10.js
@@ -1,8 +1,40 @@
+'use strict';
 var converter = require('./../lib/byte-converter').converterBase10;
 
 var assert = require("assert");
 
-describe('Unit Converter base 10', function() {
+describe('Unit Converter base 10', function () {
+    describe('Testing KB Unit', function () {
+        it('Should return 1000 B. Initial value: 1 KB', function (done) {
+            var result = converter(1, 'KB', 'B');
+            assert.equal(result, 1000);
+            done();
+        });
+
+        it('Should return 0.001 MB. Initial value: 1 KB', function (done) {
+            var result = converter(1, 'KB', 'MB');
+            assert.equal(result, 0.001);
+            done();
+        });
+
+        it('Should return 1 000 000 KB. Initial value: 1 GB', function (done) {
+            var result = converter(1, 'GB', 'KB');
+            assert.equal(result, 1000000);
+            done();
+        });
+
+        it('Should return 1000 B. Initial value: 1 KB', function (done) {
+            var result = converter(1, 'KB', 'MB');
+            assert.equal(result, 0.001);
+            done();
+        });
+    });
+    it('Should return 1000 B. Initial value: 1 KB', function (done) {
+        var result = converter(1, 'KB', 'B');
+        assert.equal(result, 1000);
+        done();
+    });
+
     it('Should return 25000 MB. Initial value: 25 GB', function (done) {
         var result = converter(25, 'GB', 'MB');
         assert.equal(result, 25000);
@@ -16,7 +48,7 @@ describe('Unit Converter base 10', function() {
     });
 
     it('Should throw an Error (Empty strings)', function (done) {
-        var fn = function(){
+        var fn = function () {
             converter(0, '', '');
         };
         assert.throws(fn, Error, "Unit must be set.");
@@ -24,7 +56,7 @@ describe('Unit Converter base 10', function() {
     });
 
     it('Should throw an Error (undefined strings)', function (done) {
-        var fn = function(){
+        var fn = function () {
             converter(0);
         };
         assert.throws(fn, Error, "Unit must be set.");
